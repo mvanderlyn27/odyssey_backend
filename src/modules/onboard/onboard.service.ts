d15f82aef730e6209c48da3f9c9d39100633ff58
michael <mvanderlyn27@gmail.com>
2025-09-30 04:54:25 -0400

    build fixes

--- src/modules/onboard/onboard.service.ts
+++ src/modules/onboard/onboard.service.ts
@@ -112,6 +112,7 @@ export const handleOnboarding = async (
         actual_reps: data.rank_exercise_reps,
         actual_weight_kg: data.rank_exercise_weight_kg,
         is_warmup: false,
+        is_amrap: false,
         is_success: true,
         is_min_success: true,
         rest_seconds_taken: 0,
